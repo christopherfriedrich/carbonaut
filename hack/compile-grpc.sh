009421200aefb638a9f1da8fbe7b85cd22a176fb
leonardpahlke <leonard.pahlke@googlemail.com>
2022-10-22 14:17:36 +0200

    update compile-grpc
    Signed-off-by: leonardpahlke <leonard.pahlke@googlemail.com>

---
+++ hack/compile-grpc.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2022 CARBONAUT AUTHOR
+#
+# Licensed under the MIT license: https://opensource.org/licenses/MIT
+# Permission is granted to use, copy, modify, and redistribute the work.
+# Full license information available in the project LICENSE file.
+
+set -euo pipefail
+
+# Required: authentication to container registry
+
+echo "Generate gRPC files for carbonaut-api"
+
+protoc -I=api/v1 --go_out=pkg/api --go_opt=paths=source_relative --go-grpc_out=pkg/api --go-grpc_opt=paths=source_relative api/v1/api.proto
+
+protoc -I=api/v1 --go_out=pkg/api --go_opt=paths=source_relative --go-grpc_out=pkg/api --go-grpc_opt=paths=source_relative api/v1/*/*.proto
+
+mkdir -p ui/packages/@carbonaut-cloud-api/dist
+
+echo "Generate gRPC files for carbonaut-ui"
+
+protoc -I=api/v1 --js_out=import_style=commonjs,binary:./ui/packages/@carbonaut-cloud-api/dist --grpc-web_out=import_style=typescript,mode=grpcweb:./ui/packages/@carbonaut-cloud-api/dist api/v1/api.proto
+protoc -I=api/v1 --js_out=import_style=commonjs,binary:./ui/packages/@carbonaut-cloud-api/dist --grpc-web_out=import_style=typescript,mode=grpcweb:./ui/packages/@carbonaut-cloud-api/dist api/v1/*/*.proto
+
+# NOTE: Workaround to make generated files usable as dependency
+echo "export * from './ApiServiceClientPb';\nexport * from './api_pb';\r" > ui/packages/@carbonaut-cloud-api/dist/index.ts
+
