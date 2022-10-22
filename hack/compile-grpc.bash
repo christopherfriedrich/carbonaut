d8ecea2ab16f6c2d7b712cf7e657dfb9ad1504d8
leonardpahlke <leonard.pahlke@googlemail.com>
2022-10-22 14:46:38 +0200

    update shebang and other formatting of commands hack files
    Signed-off-by: leonardpahlke <leonard.pahlke@googlemail.com>

--- hack/compile-grpc.bash
+++ hack/compile-grpc.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Copyright (c) 2022 CARBONAUT AUTHOR
 #
@@ -17,17 +17,16 @@ set -o pipefail
 
 echo "Generate gRPC files for carbonaut-api"
 
-protoc -I=api/v1 --go_out=pkg/api --go_opt=paths=source_relative --go-grpc_out=pkg/api --go-grpc_opt=paths=source_relative api/v1/api.proto
+protoc -I=api\/v1 --go_out=pkg\/api --go_opt=paths=source_relative --go-grpc_out=pkg\/api --go-grpc_opt=paths=source_relative api\/v1\/api.proto
 
-protoc -I=api/v1 --go_out=pkg/api --go_opt=paths=source_relative --go-grpc_out=pkg/api --go-grpc_opt=paths=source_relative api/v1/*/*.proto
+protoc -I=api\/v1 --go_out=pkg\/api --go_opt=paths=source_relative --go-grpc_out=pkg\/api --go-grpc_opt=paths=source_relative api\/v1\/*\/*.proto
 
-mkdir -p ui/packages/@carbonaut-cloud-api/dist
+mkdir -p ui\/packages\/@carbonaut-cloud-api\/dist
 
 echo "Generate gRPC files for carbonaut-ui"
 
-protoc -I=api/v1 --js_out=import_style=commonjs,binary:./ui/packages/@carbonaut-cloud-api/dist --grpc-web_out=import_style=typescript,mode=grpcweb:./ui/packages/@carbonaut-cloud-api/dist api/v1/api.proto
-protoc -I=api/v1 --js_out=import_style=commonjs,binary:./ui/packages/@carbonaut-cloud-api/dist --grpc-web_out=import_style=typescript,mode=grpcweb:./ui/packages/@carbonaut-cloud-api/dist api/v1/*/*.proto
+protoc -I="api/v1" --js_out="import_style"="commonjs,binary:./ui/packages/@carbonaut-cloud-api/dist" --grpc-web_out="import_style"="typescript,mode"="grpcweb:./ui/packages/@carbonaut-cloud-api/dist" api/v1/api.proto
+protoc -I="api/v1" --js_out="import_style"="commonjs,binary:./ui/packages/@carbonaut-cloud-api/dist" --grpc-web_out="import_style"="typescript",mode="grpcweb:./ui/packages/@carbonaut-cloud-api/dist" api/v1/*/*.proto
 
 # NOTE: Workaround to make generated files usable as dependency
 echo "export * from './ApiServiceClientPb';\nexport * from './api_pb';\r" > ui/packages/@carbonaut-cloud-api/dist/index.ts
-
